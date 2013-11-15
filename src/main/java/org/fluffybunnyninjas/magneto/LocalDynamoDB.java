ead5c2132b9a75ee19dd0abb722cd8d38e2b5f62
Daniel Raniz Raneland <daniel.raneland@sonymobile.com>
2013-11-15 13:50:43 +0100

    Added DynamoDB implementation based on SQLite
    LocalDynamoDB relies on Amazon's DynamoDBLocal for the actual
    implementation and only provides a wrapper that creates and handles a
    temporary SQLite database.
    
    Included is a repacked DynamoDBLocal.jar without all the dependencies
    needed to run it as a web application as well as sqlite4java since
    pulling it from maven messes with the native libraries.

---
+++ src/main/java/org/fluffybunnyninjas/magneto/LocalDynamoDB.java
@@ -0,0 +1,49 @@
+package org.fluffybunnyninjas.magneto;
+
+
+import com.amazonaws.regions.Region;
+import com.amazonaws.services.dynamodbv2.local.shared.access.LocalDBClient;
+import com.amazonaws.services.dynamodbv2.local.shared.access.sqlite.SQLiteDBAccess;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.util.Random;
+
+public class LocalDynamoDB extends LocalDBClient {
+
+    private final File databaseFile;
+    private final boolean deleteOnShutdown;
+
+    public LocalDynamoDB() {
+        this(new BigInteger(130, new Random()).toString(32), true);
+    }
+
+    public LocalDynamoDB(String filename, boolean deleteOnShutdown) {
+        // TODO: Replace with H2 implementation
+        super(new SQLiteDBAccess(filename));
+        // Schedule removal of the temporary database
+        this.databaseFile = new File(filename);
+        this.deleteOnShutdown = deleteOnShutdown;
+        if(deleteOnShutdown) {
+            databaseFile.deleteOnExit();
+        }
+    }
+
+    @Override
+    public void setEndpoint(String endpoint) throws IllegalArgumentException {
+        // LocalDBClient throws as error here, we just want to ignore it
+    }
+
+    @Override
+    public void setRegion(Region region) throws IllegalArgumentException {
+        // LocalDBClient throws as error here, we just want to ignore it
+    }
+
+    @Override
+    public void shutdown() {
+        super.shutdown();
+        if(deleteOnShutdown) {
+            databaseFile.delete();
+        }
+    }
+}
