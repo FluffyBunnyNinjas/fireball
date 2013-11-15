3e2b4a032b67bc44c519d69628aeec4ce929b0f3
Daniel Raniz Raneland <raniz@raneland.se>
2013-11-15 13:50:43 +0100

    Added DynamoDB implementation based on SQLite
    LocalDynamoDB relies on Amazon's DynamoDBLocal for the actual
    implementation and only provides a wrapper that creates and handles a
    temporary SQLite database.
    
    Included is a repacked DynamoDBLocal.jar without all the dependencies
    needed to run it as a web application as well as sqlite4java since
    pulling it from maven messes with the native libraries.

---
+++ src/test/java/org/fluffybunnyninjas/magneto/LocalDynamoDBTest.java
@@ -0,0 +1,11 @@
+package org.fluffybunnyninjas.magneto;
+
+import org.junit.Test;
+
+public class LocalDynamoDBTest {
+
+    @Test
+    public void testSomething() {
+        // TODO do what the method name says
+    }
+}
