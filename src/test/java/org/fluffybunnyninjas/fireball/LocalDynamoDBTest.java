cbe6d0bc485baea50030a7b79a986b1a8576d38f
Daniel Raniz Raneland <daniel.raneland@sonymobile.com>
2013-11-15 14:35:57 +0100

    Renamed LocalDynamoDB Fireball and fixed indentation

--- src/test/java/org/fluffybunnyninjas/fireball/LocalDynamoDBTest.java
+++ src/test/java/org/fluffybunnyninjas/fireball/LocalDynamoDBTest.java
@@ -4,8 +4,8 @@ import org.junit.Test;
 
 public class LocalDynamoDBTest {
 
-    @Test
-    public void testSomething() {
-        // TODO do what the method name says
-    }
+	@Test
+	public void testSomething() {
+		// TODO do what the method name says
+	}
 }
